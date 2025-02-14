b9f2f286a6db7cbf045ce062bee2525caa1072b4
jameswattsportfolio <jamiewatts5269@gmail.com>
2025-02-14 13:34:38 +0000

    Deal 4 cards (one face down)

--- screen.py
+++ screen.py
@@ -77,31 +77,34 @@ coordinate_finder_y = ctk.CTkSlider(root,
                                     width=526)
 coordinate_finder_y.pack()
 
+global card_images
+card_images = []
 # Establish the card images
 for card in initialise_deck():
     card_image = ImageTk.PhotoImage(
         Image.open(cur_dir + f"/images/Playing Cards/{card}.png").resize(
             (60, 85)), (60, 85))
-    blackjack_canvas.create_image(426,
-                                  330,
-                                  image=card_image,
-                                  anchor="nw",
-                                  tag=card,
-                                  state="hidden")
+    card_images.append(card_image)
 
 face_down_card = ImageTk.PhotoImage(
     Image.open(cur_dir + f"/images/Playing Cards/face_down.png").resize(
         (60, 85)), (60, 85))
 
-# Three face down car
-for i in range(3):
-    face_down_type = "deal" if i == 0 else "dealer" if i == 1 else "double"
-    blackjack_canvas.create_image(426,
-                                  330,
-                                  image=face_down_card,
-                                  anchor="nw",
-                                  tag=f"face_down_{face_down_type}",
-                                  state="hidden")
+# # Three face down car
+# for i in range(3):
+#     face_down_type = "deal" if i == 0 else "dealer" if i == 1 else "double"
+# blackjack_canvas.create_image(426,
+#                               330,
+#                               image=face_down_card,
+#                               anchor="nw",
+#                               tag=f"face_down_dealer",
+#                               state="hidden")
+# blackjack_canvas.create_image(426,
+#                               330,
+#                               image=face_down_card,
+#                               anchor="nw",
+#                               tag=f"face_down_double",
+#                               state="hidden")
 
 global bet_amount
 bet_amount, balance = 100, 1000
@@ -195,33 +198,68 @@ random.shuffle(deck_in_play)
 def deal_cards_animation(cards, dest_x, dest_y, i=0):
     card = cards[0]
     tag = "face_down_deal"
-    x = 426
     if i == 0:
-        blackjack_canvas.moveto(tag, x, 0)
+        blackjack_canvas.moveto(tag, dest_x, 0)
         blackjack_canvas.itemconfigure(tag, state="normal")
-        root.after(10, deal_cards_animation, cards, dest_x, dest_y, i + 1)
+        root.after(7, deal_cards_animation, cards, dest_x, dest_y, i + 1)
     elif i == 100:
-        blackjack_canvas.moveto(tag, x, 330)
+        # Hide the dealer's fist card
+        card_state = "hidden" if len(cards) == 3 else "normal"
+
+        blackjack_canvas.moveto(tag, dest_x, dest_y)
         blackjack_canvas.itemconfigure(tag, state="hidden")
-        blackjack_canvas.moveto(card, dest_x, dest_y)
-        blackjack_canvas.itemconfigure(card, state="normal")
-        root.after(10, deal_cards_animation, cards[1:], dest_x, dest_y, 0)
+
+        global card_images
+        card_image_index = initialise_deck().index(card)
+
+        if len(cards) != 3:
+            blackjack_canvas.create_image(dest_x,
+                                          dest_y,
+                                          image=card_images[card_image_index],
+                                          anchor="nw",
+                                          tag=card,
+                                          state=card_state)
+        else:
+            blackjack_canvas.create_image(dest_x,
+                                          dest_y,
+                                          image=face_down_card,
+                                          anchor="nw",
+                                          tag="face_down_dealer")
+
+        if len(cards) > 1:
+            root.after(7, deal_cards_animation, cards[1:], dest_x + 10,
+                       dest_y - 10, 0)
     else:
-        blackjack_canvas.moveto(tag, x, 0 + (3.3 * i))
-        root.after(10, deal_cards_animation, cards, dest_x, dest_y, i + 1)
+        blackjack_canvas.moveto(tag, dest_x, 0 + (3.3 * i))
+        root.after(7, deal_cards_animation, cards, dest_x, dest_y, i + 1)
 
 
-def reveal_card_at(card, x, y):
-    print("Revealing card...")
-    print(card, x, y)
-    blackjack_canvas.moveto(card, x, y)
-    blackjack_canvas.itemconfigure(card, state="normal")
+# def reveal_card_at(card, x, y):
+#     print("Revealing card...")
+#     print(card, x, y)
+#     blackjack_canvas.moveto(card, x, y)
+#     blackjack_canvas.itemconfigure(card, state="normal")
+
+face_down_card = ImageTk.PhotoImage(
+    Image.open(cur_dir + f"/images/Playing Cards/face_down.png").resize(
+        (60, 85)), (60, 85))
+
+blackjack_canvas.create_image(426,
+                              330,
+                              image=face_down_card,
+                              anchor="nw",
+                              tag="face_down_deal",
+                              state="hidden")
 
 
 def begin_game():
     global deck_in_play
     player_hands, dealer_hand, deck_in_play = deal_initial_hand(deck_in_play)
-    deal_cards_animation(player_hands[0] + dealer_hand, 426, 330)
+
+    cards_to_deal = [card for hand in player_hands
+                     for card in hand] + dealer_hand
+
+    deal_cards_animation(cards_to_deal, 426, 330)
 
 
 deal_button = ctk.CTkButton(root,
